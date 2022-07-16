6eafedae1d406f81de3fb896e1064fbf6df99d84
dakeong <109408255+dakeong@users.noreply.github.com>
2022-07-16 16:22:45 +0900

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.instagram.com/dakeong_810/" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["blue", "black"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "이다경")
+write("description", ".")
+write("button", "instgram")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "소속": "경서중학교",
+  "생년월일": "2007.08.10",
+  "해본거": "스크래치. 엔트리. c언어. 파이썬 등",
+  "좋아하는 색": "파랑색. 검은색"
 }
 information(informations)
